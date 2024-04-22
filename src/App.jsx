3b595a337688b4de49ee0a40b546a5e8d7a810b5
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-04-22 21:49:10 +0200

    fix-name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Profile from "./components/Profile/Profile";
 import userData from "./userData.json";
 import "modern-normalize";
 import friends from "./Friends.json";
-import FriendList from "./components/FriendList/FriendList";
+import FriendList from "./components/FriendList/FriendListItem";
 import transactions from "./transactions.json";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
